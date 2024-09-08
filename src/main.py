8a833c03c5764d91603ebebf35ac43e0b6e752a8
georgii <goqwertys@gmail.com>
2024-09-08 13:21:16 +0400

    main/ empty do_GET and  do_POST

--- src/main.py
+++ src/main.py
@@ -7,16 +7,18 @@ server_port = 8080
 class MyServer(BaseHTTPRequestHandler):
     """ A class that is responsible for processing incoming requests from clients """
     def do_GET(self):
+        """ Method for handling incoming GET requests """
         pass
 
     def do_POST(self):
+        """ Method for handling POST requests """
         pass
 
 if __name__ == '__main__':
     # Initialization of a web server that will accept requests in the
     # network according to the specified parameters and send them for processing to a special class,
     # which was described above
-    web_server = HTTPServer((host_name, server_port),MyServer):
+    web_server = HTTPServer((host_name, server_port),MyServer)
     print("Server started http://%s:%s" % (host_name, server_port))
 
     try:

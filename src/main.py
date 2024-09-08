103c03dbf2ffa426fe8884c727237a9c9095df67
georgii <goqwertys@gmail.com>
2024-09-08 13:13:17 +0400

    structure

---
+++ src/main.py
@@ -0,0 +1,28 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+# import time
+
+host_name = 'localhost'
+server_port = 8080
+
+class MyServer(BaseHTTPRequestHandler):
+    """ A class that is responsible for processing incoming requests from clients """
+    def do_GET(self):
+        pass
+
+    def do_POST(self):
+        pass
+
+if __name__ == '__main__':
+    # Initialization of a web server that will accept requests in the
+    # network according to the specified parameters and send them for processing to a special class,
+    # which was described above
+    web_server = HTTPServer((host_name, server_port),MyServer):
+    print("Server started http://%s:%s" % (host_name, server_port))
+
+    try:
+        web_server.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    web_server.server_close()
+    print('Server stopped')
